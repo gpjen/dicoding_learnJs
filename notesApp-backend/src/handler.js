9aecd9940737f0ab1ecb2a356ce594615ec9e9a9
agan95 <nyonggpj9995@gmail.com>
2022-07-08 13:45:40 +0900

    keep learning

--- notesApp-backend/src/handler.js
+++
@@ -1,9 +0,0 @@
-exports.notes = {
-  creteNote: (req, h) => {
-    const { name, address } = req.payload;
-    return name + " " + address;
-  },
-  getNotes: (req, h) => {
-    return "datanya notes";
-  },
-};
